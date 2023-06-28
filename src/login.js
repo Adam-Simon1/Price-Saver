ade6c8a1e180d5045c709f1a480c1c9da6d44fa3
Adam Simon <adamsimon488@gmail.com>
2023-06-28 15:18:36 +0200

    Modified config for deployment

--- src/login.js
+++ src/login.js
@@ -56,6 +56,7 @@ app.get("/saved-tables", (req, res) => {
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
@@ -417,4 +418,4 @@ app.post("/send-array", authUser.authenticateToken, (req, res) => {
   res.json({ array });
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
